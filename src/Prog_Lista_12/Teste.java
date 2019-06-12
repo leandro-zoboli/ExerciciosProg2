8c5dcde447d0e1d6a5524b2bb80c0a6ba6d45547
leandroz <leandroz@MC-5180.furb.br>
2019-06-11 21:35:46 -0300

    papa

---
+++ src/Prog_Lista_12/Teste.java
@@ -0,0 +1,10 @@
+package Prog_Lista_12;
+
+public class Teste {
+    
+    public static void main(String[] args) {
+        Sanduiche x = new XBacon();
+        System.out.println(x.toString() + "\r\nPreço: " + x.getPreco());
+    }
+    
+}
